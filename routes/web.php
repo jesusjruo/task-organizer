68b401bffa3603f7ca67fb405675c02fca5677b7
Jesus Urdaneta <jesusjruo@gmail.com>
2024-02-28 10:11:14 -0600

    Create tasks and other setup steps

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Models\Task;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return redirect()->route('tasks.index');
 });
+
+// Using groups for controllers (TaskController)
+Route::controller(TaskController::class)->group(function ()  {
+  Route::get('/tasks','viewAllTasks')->name('tasks.index');
+  Route::view('/tasks/create','create')->name('tasks.create');
+  Route::get('/tasks/{task}' , 'viewTask')->name('tasks.show');
+  Route::post('/tasks' , 'createTask')->name('tasks.store');
+});
+
+// Without using groups for controller
+// Route::get('/tasks', [TaskController::class , 'viewAllTasks'])->name('tasks');
+// Straight view route (This is better to avoid create an endpoint in the controller for just a view call)
+// Route::view('/tasks/create' , 'create')->name('tasks.create');
+// Route::get('/tasks/{id}', [TaskController::class , 'viewTask'])->name('tasks.show');
+// Route::post('/tasks' , [TaskController::class ,'createTask'])->name('tasks.store');
+
+//The method fallback() from the class Route allows us to do something if the URL doesn't match any route.
+Route::fallback(function() {
+    return redirect('/');
+});
+
+//php artisan tinker let's run laravel queries in the console
